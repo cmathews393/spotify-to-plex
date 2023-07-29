4376cfd8f5406a16d38ec12bb5eeb7df79c4b8fb
ChrisMathews393 <christophermathews393@gmail.com>
2023-07-29 02:11:47 -0600

    Added some brief comments

--- spotify-playlist-importer.py
+++ spotify-playlist-importer.py
@@ -4,29 +4,29 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-SPOTIPY_CLIENT_ID = input('Spotify Client ID: ')
-SPOTIPY_CLIENT_SECRET = input('Spotify Secret: ')
 
-PLEX_SERVER_TOKEN = input('PLEX Server Token: ')
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #Plex API hates SSL or something idk it was annoying
+
+SPOTIPY_CLIENT_ID = input('Spotify Client ID: ') 
+SPOTIPY_CLIENT_SECRET = input('Spotify Secret: ')
+keepgoing = True #Used later for loop
+PLEX_SERVER_TOKEN = input('PLEX Server Token: ') 
 PLEX_SERVER_URL = input('Server URL, include http(s) and port, eg https://192.168.1.2:32400: ')
 
-session = requests.Session()
-session.verify = False
-plex = PlexServer(PLEX_SERVER_URL, PLEX_SERVER_TOKEN, session=session)
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET))
+session = requests.Session() #Open session
+session.verify = False #Ignore SSL errors
+plex = PlexServer(PLEX_SERVER_URL, PLEX_SERVER_TOKEN, session=session) #Connect to Plex server
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)) #Connect to Spotify API
 
-def extract_playlist_id(playlist_url):
+def extract_playlist_id(playlist_url): #parse playlist ID from URL if applicable
     if "playlist/" in playlist_url:
         playlist_id = playlist_url.split("playlist/")[1]
         return playlist_id
     else:
         playlist_id = playlist_url
         return playlist_id
-    
 
-    
-def get_spotify_playlist_tracks(playlist_id):
+def get_spotify_playlist_tracks(playlist_id): #Get all tracks from Spotify, check Plex to see if they exist, and then add them to a list for Plex to use in create_list
     results = sp.playlist_tracks(playlist_id)
     music = plex.library.section('Music')
     plex_tracks = []
@@ -46,14 +46,12 @@ def get_spotify_playlist_tracks(playlist_id):
             break
     return plex_tracks
 
-def getplaylistname(playlist_id):
+def getplaylistname(playlist_id): #Get the name of the playlist so create_list can use it
 
     playlistname = sp.playlist(playlist_id, fields=["name"])
     playlist_name = playlistname["name"]
     return playlist_name
-
-        
-
+    
 def create_list(plextracks,playlist_name):
     plex_playlist = plex.createPlaylist(title=playlist_name, items=plextracks)
     #pms.createPlaylist(title="testing")
@@ -62,7 +60,7 @@ def create_list(plextracks,playlist_name):
     else:
         print("Failed to create the playlist.")
         return(0)
-keepgoing = True
+
 while keepgoing == True:
     try:
         PLAYLIST_url = input('Playlist ID or url: ')
