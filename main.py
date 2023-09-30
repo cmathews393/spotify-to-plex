f770e14813709549cceac07cb467a97805b61ff5
root <christophermathews393@gmail.com>
2023-09-30 15:05:20 -0600

    configured multithreading added performance tracking (execution time lol)

--- main.py
+++ main.py
@@ -2,42 +2,74 @@ from spotiplex import (
 connect_plex,
 connect_spotify, 
 getlidarrlists, 
-extract_playlist_id, 
-get_playlist_name,
-get_spotify_playlist_tracks,
-create_list,
-check_tracks_in_plex
+# extract_playlist_id, 
+# get_playlist_name,
+# get_spotify_playlist_tracks,
+# create_list,
+# check_tracks_in_plex,
+process_playlist
 )
 
-from decouple import config
+import time
+
+import concurrent.futures
+
+from concurrent.futures import ThreadPoolExecutor
+
+
+# def main():
+    
+#     # Assuming you have this in your environment variables or settings
+#     plex = connect_plex()
+#     sp = connect_spotify()
+
+#     try:
+#         lidarr_playlists = getlidarrlists()
+
+#         for playlist in lidarr_playlists:
+#             try:
+#                 playlist_id = extract_playlist_id(playlist)
+#                 spotify_tracks = get_spotify_playlist_tracks(sp, playlist_id)
+#                 plex_tracks, _ = check_tracks_in_plex(plex, spotify_tracks)
+#                 playlist_name = get_playlist_name(sp, playlist_id)
+
+#                 create_list(plex, plex_tracks, playlist_name)
+
+#                 print(f"Processed playlist '{playlist_name}'.")
+
+#             except Exception as e:
+#                 print(f"An error occurred processing playlist {playlist}:", e)
+#                 continue  # continue will ensure that the loop proceeds to the next playlist even if an error occurs
+#     except Exception as playlistfailed:
+#         print("Grabbing all playlists failed")
+#         print(playlistfailed)
 
 def main():
-    
-    # Assuming you have this in your environment variables or settings
+    start = time.time()
     plex = connect_plex()
     sp = connect_spotify()
 
-    try:
-        lidarr_playlists = getlidarrlists()
+    lidarr_playlists = getlidarrlists()
 
-        for playlist in lidarr_playlists:
-            try:
-                playlist_id = extract_playlist_id(playlist)
-                spotify_tracks = get_spotify_playlist_tracks(sp, playlist_id)
-                plex_tracks, _ = check_tracks_in_plex(plex, spotify_tracks)
-                playlist_name = get_playlist_name(sp, playlist_id)
-
-                create_list(plex, plex_tracks, playlist_name)
-
-                print(f"Processed playlist '{playlist_name}'.")
-
-            except Exception as e:
-                print(f"An error occurred processing playlist {playlist}:", e)
-                continue  # continue will ensure that the loop proceeds to the next playlist even if an error occurs
-    except Exception as playlistfailed:
-        print("Grabbing all playlists failed")
-        print(playlistfailed)
+    # Use ThreadPoolExecutor to process multiple playlists simultaneously
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        # Use 'executor.submit' to start the function in a new thread
+        futures = [executor.submit(process_playlist, playlist, plex, sp) for playlist in lidarr_playlists]
         
+        # Wait for all tasks to complete. If you want to handle exceptions or 
+        # gather results returned by the tasks, you can do that here.
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                # This will raise an exception if the function erred
+                future.result()
+            except Exception as e:
+                print(f"Thread resulted in an error: {e}")
+    end = time.time()
+    # print the difference between start
+    # and end time in milli. secs
+    print("The time of execution of above program is :",
+        (end-start) * 10**3, "ms")
+
 
 
 if __name__ == "__main__":
