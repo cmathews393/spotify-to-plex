84e2a1962e9bf1e881fc79d9bd3a202bea7a2abf
0xChloe <chloe@catastrophesoftware.com>
2024-01-27 04:01:45 +0000

    initial commit of rewrite

---
+++ spotiplex/confighandler.py
@@ -0,0 +1,28 @@
+import rtoml
+import os
+
+config_file = "config.toml"
+
+
+def ensure_config_exists():
+    """Ensure the configuration file exists, and create it with default values if it doesn't."""
+    if not os.path.exists(config_file):
+        with open(config_file, "w") as file:
+            rtoml.dump({}, file)
+
+
+def read_config(service):
+    ensure_config_exists()  # Check if config file exists
+    with open(config_file, "r") as file:
+        config = rtoml.load(file)
+    print(config)
+    return config.get(service, {})
+
+
+def write_config(service, data):
+    ensure_config_exists()  # Check if config file exists
+    with open(config_file, "r") as file:
+        config = rtoml.load(file)
+    config[service] = data
+    with open(config_file, "w") as file:
+        rtoml.dump(config, file)
