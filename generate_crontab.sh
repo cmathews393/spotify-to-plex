695bff514cc9bc1f9a171589a0609337b5f0b784
0xChloe <chloe@catastrophesoftware.com>
2024-06-22 07:44:17 +0000

    file cleanup

--- generate_crontab.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Generate the cron file based on environment variables
-echo "${CRON_SCHEDULE} poetry run spotiplex sync-lidarr-imports" > /etc/supercronic-cron
-echo "${CRON_SCHEDULE} poetry run spotiplex sync-manual-lists" > /etc/supercronic-cron
